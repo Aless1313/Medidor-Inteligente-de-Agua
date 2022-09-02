3360296fa9d45204fd82a33d6bb75268bf57aae2
Alessandro Elizondo <alessandro.uanl@gmail.com>
2022-09-02 18:38:18 -0500

    WiFi-MQTT
    Se realizo conexión a wifi desde código y se logró enviar datos mediante protocolo mqtt utilizando mqtx

--- Medidor Inteligente de Agua/src/main.cpp
+++ Medidor Inteligente de Agua/src/main.cpp
@@ -1,4 +1,22 @@
 #include <Arduino.h>
+#include <PubSubClient.h>
+#include <ESP8266Wifi.h>
+
+const char* ssid = "Fisica Pato2";
+const char* password = "q1w2e3r4t5";
+
+const char* mqtt_server = "broker.emqx.io";
+const int mqtt_port = 1883;
+const char* mqtt_user ="admin";
+const char* mqtt_pass = "public";
+
+int led = 16;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[25];
+char msg2[25];
 
 /*Sensor de flujo de agua*/
 #define SENSORAGUA 14
@@ -18,6 +36,9 @@ unsigned long totalMilliLitres;
 float flowLitres;
 float totalLitres;
 
+void setup_wifi();
+void callback(char* topic, byte* payload, int length);
+void reconnect();
 
 
 void IRAM_ATTR pulseCounter(){
@@ -43,11 +64,22 @@ void setup() {
     Serial.println("wifi");
   }
 
+  setup_wifi();
+
+  client.setServer(mqtt_server,mqtt_port);
+  client.setCallback(callback);
+
   attachInterrupt(digitalPinToInterrupt(SENSORAGUA), pulseCounter, FALLING);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
+
+  if(!client.connected()){
+    reconnect();
+  }
+  client.loop();
   // put your main code here, to run repeatedly:
   currentMillis = millis();
 
@@ -61,17 +93,90 @@ void loop() {
      flowLitres = (flowRate/60);
     totalLitres += flowLitres;
 
-    Serial.print("Flow Rate: ");
+    Serial.print("Flujo de agua: ");
     Serial.print(float(flowRate));
     Serial.print("L/Min");
     Serial.print("\t");
 
-    Serial.print("Output Liquid Quantify: ");
+    Serial.print("Litros consumidos: ");
     Serial.print(totalLitres);
     Serial.println("L");
 
-    digitalWrite(LED_BUILTIN, HIGH);
+    String flujo = String(flowRate, 3); 
+    String to_Send = "F:" + flujo + " L:" + totalLitres;
+    to_Send.toCharArray(msg, 25);
+    client.publish("values", msg);
+    Serial.println("Enviado a topico");
+
+    String flujo2 = flujo;
+    flujo2.toCharArray(msg2, 20);
+    client.publish("flujo", msg2);
+  }
+    
+  /*
+  if(currentMillis - previousMillis > 5000){
+    String to_Send = "Test";
+    to_Send.toCharArray(msg, 25);
+
+    Serial.println("Enviado a topico");
+    client.publish("values", msg);
+  }*/
+
+}
+
+void setup_wifi(){
+  delay(10);
+  Serial.println("");
+  Serial.print("Conectando a.. ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(400);
+    Serial.print(".");
   }
 
-  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("");
+  Serial.print("Conectado con IP..");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, int length ){
+  String incoming = "";
+  Serial.print("Mensaje recibido de ->");
+  Serial.print(topic);
+  Serial.println("");
+
+  for(int i=0; i<length; i++){
+    incoming += (char)payload[i];
+  }
+
+  incoming.trim();
+  Serial.println("Mensaje -> " + incoming);
+
+  if(incoming == "ON"){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  
+  if(incoming == "OFF"){
+      digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void reconnect(){
+  while(!client.connected()){
+    Serial.print("Conectando a MQTT...");
+    String clientId = "ESP8266";
+    clientId += String(random(0xffff), HEX);
+
+    if(client.connect(clientId.c_str(), mqtt_user, mqtt_pass)){
+      Serial.println("Conectado a mqtt");
+      client.subscribe("led");
+      Serial.println("Subscrito a topico");
+    }else{
+      Serial.println("Fallo ->" + client.state());
+      delay(2500);
+    }
+  }
 }
